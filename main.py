6e4b3231f9fa2ff138283e9be0871555bc9cd923
bling05 <brantonling05@gmail.com>
2022-11-03 12:17:44 -0400

    comments

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import time
-import random
-
-board = [['_', '_', '_'], 
-         ['_', '_', '_'], 
-         ['_', '_', '_']]
-
 
 board_coors = {'TL':[0,0], 'T':[0,1], 'TR':[0,2], # Lookup table to convert player input to board indices
                'L': [1,0], 'C':[1,1], 'R': [1,2],
@@ -13,8 +7,8 @@ board_coors = {'TL':[0,0], 'T':[0,1], 'TR':[0,2], # Lookup table to convert play
 scores = {'tie': 0, 'X': 1, 'O': -1} # X is always the MAXIMIZING player, O is always MINIMIZING
 
 def main():
-    global board
-    while True:
+    board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']] # Initializes/resets board
+    while True: 
         global player
         player = input("Would you like to be X or O? X goes first. ").upper()
         if player == 'X':
@@ -30,10 +24,10 @@ def main():
     next_turn = True
     while next_turn:
         if player_to_move == player:
-            print_board()
+            print_board(board)
             move = input("\nMake your move:\nTL T TR\nL  C R\nBL B BR\n\n").upper()
             if move in board_coors:
-                row = board_coors[move][0]
+                row = board_coors[move][0] # Uses board_coors dict to make move on board (if it exists)
                 col = board_coors[move][1]
                 if board[row][col] == '_': 
                     board[row][col] = player
@@ -41,82 +35,78 @@ def main():
             else: continue
 
             player_to_move = ai
-            print_board()
+            print_board(board)
             time.sleep(0.5)
             print('\nAI is moving...')
 
         else:
-            best_move()
+            best_move(board)
             player_to_move = player
 
-        if check_winner() != None:
-            print_board()
-            declare_result(check_winner())
-            board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+        result = check_winner(board)
+        if result: # If there is an endgame state, announce and reset loop
+            print_board(board)
+            declare_result(result)
             time.sleep(0.5)
             next_turn = False
                 
 
-def best_move():
-    if ai == 'X':
-        best_score = -1000 # Sets initial best score as worst case
-        other_maximizing = False # If ai is X (maximizing player), checks each tile for worst case scenario with minimax (minimum score)
-    else:
+def best_move(board):
+    ai_is_maximizing = ai == 'X'
+    if ai_is_maximizing: 
+        best_score = -1000
+    else: 
         best_score = 1000
-        other_maximizing = True
 
+    move = (0, 0)
     for row in range(3):
         for col in range(3):
             if board[row][col] == '_':
                 board[row][col] = ai
-                score = minimax(other_maximizing)
+                score = minimax(board, not ai_is_maximizing) # Calls minimax on all possible board states after AI move (minimizing if AI is maximizer, vice versa)
                 board[row][col] = '_'
-                if ai == 'X':
+
+                if ai_is_maximizing: # Selects best possible move based on whether AI is minimizing or maximizing
                     if score > best_score:
                         best_score = score
-                        best_move_row = row
-                        best_move_col = col
+                        move = (row, col)
                 else:
                     if score < best_score:
                         best_score = score
-                        best_move_row = row
-                        best_move_col = col
-                        
-    if empty_board():
-        best_move_row = random.choice([0,2])
-        best_move_col = random.choice([0,2])
+                        move = (row, col)
 
-    board[best_move_row][best_move_col] = ai
+    board[move[0]][move[1]] = ai
 
-def minimax(is_maximizing):
-    result = check_winner()
+def minimax(board, is_maximizing):
+    result = check_winner(board)
     if result:
         return scores[result] # If there is an end-game state, no need to look further!
-    if is_maximizing == True: # AI is the maximizing player X
+
+    if is_maximizing: # best move for X
         best_score = -1000
         for row in range(3):
             for col in range(3):
                 if board[row][col] == '_':
-                    board[row][col] = ai
-                    score = minimax(False)
+                    board[row][col] = 'X'
+                    score = minimax(board, False) 
                     board[row][col] = '_'
                     best_score = max(score, best_score)
                     
         return best_score
 
-    else:
+    else: # best move for O
         best_score = 1000
         for row in range(3):
             for col in range(3):
                 if board[row][col] == '_':
-                    board[row][col] = ai
-                    score = minimax(True)
+                    board[row][col] = 'O'
+                    score = minimax(board, True)
                     board[row][col] = '_'
                     best_score = min(score, best_score)
-
+                
         return best_score
 
-def empty_board():
+def empty_board(board):
     for row in board:
         for ele in row:
             if ele != '_':
@@ -126,7 +116,7 @@ def empty_board():
 def winning_row(a, b, c): # Given 3 elements in a row, determines if they are a tic-tac-toe
     return a != '_' and a == b and b == c
 
-def check_winner():
+def check_winner(board): # Returns end game state, returns None if game is ongoing
     winner = None
     for row in board:
         if winning_row(row[0], row[1], row[2]):
@@ -156,8 +146,8 @@ def declare_result(result):
     elif result == ai:
         print("AI win! The robot revolution is nigh 😈\n") 
 
-def print_board():
-    print(chr(27) + "[2J")
+def print_board(board):
+    print("\n"*10)
     for row in board:
         print(*row)
 
